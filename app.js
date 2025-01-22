28d3ffcd88cdf5dab8c1c05ca1e4aab59db058bd
sanmijingbrahma <bsanmijing@gmail.com>
2025-01-22 16:23:40 +0530

    change port 5000 to 8000

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const connectDB = require('./database/dbConnect');
 const authRoutes = require("./routes/authRoutes")
 
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8000
 const app = express();
 
 app.use(express.json());
